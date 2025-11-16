4e3086518b3e86f3a28f3e9d2fe608b308108a04
Stefan <bymyself.studio@gmail.com>
2025-11-16 14:44:14 +0100

    Added comments

--- ValidateHash.ps1
+++ ValidateHash.ps1
@@ -1,9 +1,9 @@
 # Execution: 
-# powershell.exe Validate_Hash.ps1 -Path "Path to file" -Algorithm "Algorithm" -ExpectedHash "Given hash"
+# powershell.exe ValidateHash.ps1 -Path "Path to file" -Algorithm "Algorithm" -ExpectedHash "Given hash"
 
 # TODO:
-# Add help page --> option -help     --> alias -h
-# Add version   --> option -version  --> alias -v
+# Add help page --> option --help     --> alias -h
+# Add version   --> option --version  --> alias -v
 
 param(#
     [Parameter(Mandatory, HelpMessage = "Please provide a valid path")]
@@ -20,15 +20,14 @@ function Title() {
 "
 \  / _ |. _| _ _|_ _   |_| _  _|_ 
  \/ (_|||(_|(_| | (/_  | |(_|_\| |
-                              v0.2
-
+                              v0.3
 "
 }
 
+# Typeof Object
 $FileHash = Get-FileHash $Path -Algorithm $Algorithm
 
 function Print() {
-    
     Clear-Host
     Title
     Write-Host "Calculated hash: " 
