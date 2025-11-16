651ee5d4698297c717bc4e1a0638d8d75286210a
Stefan <bymyself.studio@gmail.com>
2025-11-16 14:03:43 +0100

    Renamed parameters

--- RenameFileSet.ps1
+++ RenameFileSet.ps1
@@ -5,34 +5,34 @@
     .DESCRIPTION
     Renames multiple files of the given format type with the given name and increasing number.
 
-    .PARAMETER suffix
+    .PARAMETER Suffix
     Any kind of media format like .txt, .pdf, .jpg and so on.
 
-    .PARAMETER baseName
+    .PARAMETER BaseName
     The new name of the file(s).
 
     .EXAMPLE
-    Rename-Files.ps1 -extension ".jpg" -baseName "Vacation2023"
+    Rename-Files.ps1 -extension ".jpg" -BaseName "Vacation2023"
 #>
 
 param(
     [Parameter(Mandatory)]
     # Extension (like .txt or .pdf etc)
-    [String]$extension,  
+    [String]$Extension,  
 
     [Parameter(Mandatory)]
     # Base name for the new files (like image or doc etc.)
-    [String]$baseName 
+    [String]$BaseName 
 )
 
-$files = Get-ChildItem -Filter $suffix -File -Path .
-# $extension = $extension.Replace("*", "")
-$count = 0
+$files = Get-ChildItem -Filter $Suffix -File -Path .
+# $Extension = $Extension.Replace("*", "")
+$Count = 0
 
 foreach ($file in $files) {
-    $count = $count + 1
-    $newName = "{0}_{1}{2}" -f $baseName, $count, $extension
+    $Count = $Count + 1
+    $newName = "{0}_{1}{2}" -f $BaseName, $Count, $Extension
     Rename-Item -NewName $newName -Path $file.FullName
 }
 
-Write-Host ("`n{0} files have been renamed.`n" -f $count)
+Write-Host ("`n{0} files have been renamed.`n" -f $Count)
